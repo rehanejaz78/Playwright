4719db84e9bb75cdf8bcccd7648dc190a5e84794
rehanejaz78 <rehanejaz78@gmail.com>
2022-10-13 17:28:19 +0500

    added test

--- tests/demo/myfirstTest.spec.js
+++ tests/demo/myfirstTest.spec.js
@@ -1,1 +1,9 @@
 //this is my first playwright test.
+
+const { test, expect } = require("@playwright/test");
+
+test("My first playwright test", async ({ page }) => {
+  await page.goto("https:www.google.com");
+
+  await expect(page).toHaveTitle("Google");
+});
