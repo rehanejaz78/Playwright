15d23b6e76b128b65e543e15205e8f463c9788a8
rehanejaz78 <rehanejaz78@gmail.com>
2022-10-14 18:22:55 +0500

    login

--- tests/demo/myfirstTest.spec.js
+++ tests/demo/myfirstTest.spec.js
@@ -4,7 +4,12 @@ const { test, expect } = require("@playwright/test");
 
 //1st
 test("My first playwright test", async ({ page }) => {
-  await page.goto("https:www.google.com");
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("input[id='userEmail']").fill("email3@example.com");
 
-  await expect(page).toHaveTitle("Google");
+  await page.locator("input[id= 'userPassword']").type("Rehan@1234");
+
+  await page.locator("#login").click();
+
+  console.log("Logged in succewssfully !! ");
 });
